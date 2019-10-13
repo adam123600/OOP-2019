6bde9ef9165c64f0e8ae5c019932f39f61841759
adam123600 <adam123600@gmail.com>
2019-10-13 23:32:03 +0000

    Dummy.h created online with Bitbucket

---
+++ cwiczenia1Zad2/Dummy.h
@@ -0,0 +1,14 @@
+//
+// Created by student on 13.10.2019.
+//
+
+//#ifndef PROJECT2_DUMMY_H
+//#define PROJECT2_DUMMY_H
+
+//#endif //PROJECT2_DUMMY_H
+
+class MojaKlasa
+{
+public:
+      static int dodawanie(int x, int y);
+};
