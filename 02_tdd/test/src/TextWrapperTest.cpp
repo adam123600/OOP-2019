90319942d0dac643e7df13c7c930c2184262a616
adam123600 <adam123600@gmail.com>
2019-10-18 22:20:11 +0200

    Dorzucenie 02_tdd

---
+++ 02_tdd/test/src/TextWrapperTest.cpp
@@ -0,0 +1,14 @@
+#include "TestIncludes.h"
+#include "TextWrapper.h"
+
+TEST(TextWrapper, CreateInstance) {
+
+    auto wrapper = TextWrapper{};
+}
+
+TEST(TextWrapper, HasColumnsGetter) {
+
+    auto wrapper = TextWrapper{};
+
+    EXPECT_EQ(10, wrapper.columns());
+}
