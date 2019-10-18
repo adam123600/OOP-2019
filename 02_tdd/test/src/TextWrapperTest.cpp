9e59c27634ad680a0f2d3e9c6390368a5778a7c7
adam123600 <adam123600@gmail.com>
2019-10-18 22:55:07 +0200

    Teraz juz nic nie zrobie bo Pan Jakiela przyszedl z piwem

--- 02_tdd/test/src/TextWrapperTest.cpp
+++ 02_tdd/test/src/TextWrapperTest.cpp
@@ -1,6 +1,8 @@
 #include "TestIncludes.h"
 #include "TextWrapper.h"
 
+
+/*
 TEST(TextWrapper, CreateInstance) {
 
     auto wrapper = TextWrapper{};
@@ -11,4 +13,13 @@ TEST(TextWrapper, HasColumnsGetter) {
     auto wrapper = TextWrapper{};
 
     EXPECT_EQ(10, wrapper.columns());
+}
+
+ */
+
+TEST(TextWrapper, Test1)
+{
+    auto wrapper = TextWrapper{};
+
+    EXPECT_EQ("", )
 }
