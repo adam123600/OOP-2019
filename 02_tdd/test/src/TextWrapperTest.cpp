9cbec79891d481d3daa55071829b4f06f8e44061
adam123600 <adam123600@gmail.com>
2019-10-19 13:48:59 +0200

    (red) Dodanie testu - nie dziala

--- 02_tdd/test/src/TextWrapperTest.cpp
+++ 02_tdd/test/src/TextWrapperTest.cpp
@@ -21,5 +21,7 @@ TEST(TextWrapper, Test1)
 {
   //  auto wrapper = TextWrapper{};
     //EXPECT_EQ(5, TextWrapper::columns("ASD", 5));
-    ASSERT_EQ(5, 10);
+    //ASSERT_EQ(5, 10);
+
+    ASSERT_STREQ(" ", TextWrapper::columns("ASD", 5));
 }
