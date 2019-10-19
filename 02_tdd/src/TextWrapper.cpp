e743a1f05f1e60b91129f3f0bddff4e5c2a16f7c
adam123600 <adam123600@gmail.com>
2019-10-19 18:07:41 +0200

    (green) Dodanie nSize == 2, dziala ale zle

--- 02_tdd/src/TextWrapper.cpp
+++ 02_tdd/src/TextWrapper.cpp
@@ -1,5 +1,7 @@
 #include "TextWrapper.h"
 #include <iostream>
+//#include <stdio.h>
+#include <string.h>
 /*
 std::string TextWrapper::columns(std::string inputString, int nSize)
 {
@@ -24,12 +26,38 @@ std::__cxx11::string TextWrapper::wrapper(const char * buffer, int nSize)
 
     char* pStart = (char*)buffer;
 
+    int lenghtBuffer = strlen(buffer);
+
     while( *pStart != '\0')
     {
+        if ( nSize < 1)
+        {
+            std::cout << "Podano nieodpowiednia liczbe!" << std::endl;
+            exit(0);
+        }
+
         if ( nSize == 1)
         {
             std::cout << *pStart << std::endl;
         }
+
+        else if ( nSize == 2)
+        {
+            int halfL = lenghtBuffer / 2;
+            while ( halfL )
+            {
+                std::cout << *pStart << std::endl;
+                pStart++;
+                halfL--;
+            }
+            while( *pStart != '\0' )
+            {
+                std::cout << *pStart << std::endl;
+                pStart++;
+                exit(1);
+            }
+        }
+
         pStart++;
     }
     std::cout << std::endl;
