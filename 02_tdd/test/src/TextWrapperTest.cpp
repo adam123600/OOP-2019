93286ea8568714453c6a801e886eb79f716a53bc
adam123600 <adam123600@gmail.com>
2019-10-19 13:59:09 +0200

    (red) Odkomentowanie poprzednich testow

--- 02_tdd/test/src/TextWrapperTest.cpp
+++ 02_tdd/test/src/TextWrapperTest.cpp
@@ -2,7 +2,7 @@
 #include "TextWrapper.h"
 
 
-/*
+
 TEST(TextWrapper, CreateInstance) {
 
     auto wrapper = TextWrapper{};
@@ -14,14 +14,18 @@ TEST(TextWrapper, HasColumnsGetter) {
 
     EXPECT_EQ(10, wrapper.columns());
 }
-*/
+*
 
 
+/*
 TEST(TextWrapper, Test1)
 {
   //  auto wrapper = TextWrapper{};
     //EXPECT_EQ(5, TextWrapper::columns("ASD", 5));
     //ASSERT_EQ(5, 10);
 
-    ASSERT_STREQ(" ", TextWrapper::columns("ASD", 5));
+    //ASSERT_STREQ(" ", TextWrapper::columns("ASD", 5));
+
+    EXPECT_STRCASEEQ("ASD", TextWrapper::columns("ASD", 5));
 }
+*/
